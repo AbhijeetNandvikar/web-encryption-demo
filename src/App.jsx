a0ae4d5063933860de1eecbb903ec336078c21b6
Abhijeet Nandvikar <abhijeetvn1234@gmail.com>
2023-05-05 12:28:22 +0530

    adds functionality

---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import "./app.css";
+import React, { useState } from "react";
+import { startEncryption, startDecryption } from "./functions";
+
+const App = () => {
+  const [currentFile, setCurrentFile] = useState(null);
+  const [password, setPassword] = useState("");
+  const [iv, setIv] = useState(null);
+
+  const handleChange = (e) => {
+    if (e.target.files[0]) setCurrentFile(e.target.files[0]);
+  };
+
+  return (
+    <div className="App">
+      <h2>How to use this App?</h2>
+      <ul>
+        <li>
+          If you want to encrypt a file:
+          <ol>
+            <li>Choose a password for encryption</li>
+            <li>Add a file to encrypt</li>
+            <li>
+              Click on start encryption, the file will be encrypted and will be
+              saved on your machine.
+            </li>
+          </ol>
+        </li>
+        <br />
+        <li>
+          <ol>
+            <li>Enter the password you used while encrypting the file.</li>
+            <li>Click on Add file button add encrypted file</li>
+            <li>
+              Click on decrypt file button and decrypted file will be saved on
+              your machine
+            </li>
+          </ol>
+        </li>
+      </ul>
+      <h3 style={{ marginBottom: "32px" }}>
+        Note : Don't refresh browser because initializaton vector value is
+        stored temporarily, also after decrypting file remember to give correct
+        file extension while saving file.
+      </h3>
+      <div style={{ marginBottom: "25px" }}>
+        <label htmlFor="passwordInput">
+          Enter password for encryption / decryption :
+        </label>
+        <input
+          id="passwordInput"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="fileStagingContainer">
+        <label className="fileUploadBtn" htmlFor="fileInput">
+          Add File
+        </label>
+        <input id="fileInput" type="file" onChange={(e) => handleChange(e)} />
+        {currentFile ? (
+          <div className="fileInfoContainer">
+            <h3>File Info :</h3>
+            <div>{`Name: ` + currentFile.name}</div>
+            <div>{`Size: ` + currentFile.size}</div>
+            <div>{`Type: ` + currentFile.type}</div>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+      <div style={{ marginTop: "25px" }}>
+        <button
+          className="encryptFileBtn"
+          onClick={() => startEncryption(currentFile, password, setIv)}
+        >
+          Start Encryption
+        </button>
+        <button
+          className="decryptFileBtn"
+          onClick={() => startDecryption(currentFile, password, iv)}
+        >
+          Start Decryption
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/functions.js
@@ -0,0 +1,99 @@
+var FileSaver = require("file-saver");
+
+export const getKey = (value) => {
+  return window.crypto.subtle.importKey("raw", value, "AES-GCM", true, [
+    "encrypt",
+    "decrypt",
+  ]);
+};
+
+export const formatBytes = (bytes) => {
+  var marker = 1024; // Change to 1000 if required
+  var decimal = 3; // Change as required
+  var kiloBytes = marker; // One Kilobyte is 1024 bytes
+  var megaBytes = marker * marker; // One MB is 1024 KB
+  var gigaBytes = marker * marker * marker; // One GB is 1024 MB
+  var teraBytes = marker * marker * marker * marker; // One TB is 1024 GB
+
+  // return bytes if less than a KB
+  if (bytes < kiloBytes) return bytes + " Bytes";
+  // return KB if less than a MB
+  else if (bytes < megaBytes)
+    return (bytes / kiloBytes).toFixed(decimal) + " KB";
+  // return MB if less than a GB
+  else if (bytes < gigaBytes)
+    return (bytes / megaBytes).toFixed(decimal) + " MB";
+  // return GB if less than a TB
+  else return (bytes / gigaBytes).toFixed(decimal) + " GB";
+};
+
+export const encryptFile = async (key, iv, file) => {
+  return await window.crypto.subtle.encrypt(
+    {
+      name: "AES-GCM",
+      iv: iv,
+    },
+    key,
+    file
+  );
+};
+
+// get the iv which is similar the salt value used for encryption
+export const getiv = () => {
+  return window.crypto.getRandomValues(new Uint8Array(12));
+};
+
+// load the file in the memory
+export const getFile = async (inputFile) => {
+  return await inputFile.arrayBuffer();
+};
+
+export const decryptFile = (key, iv, cipherText) => {
+  return window.crypto.subtle.decrypt(
+    {
+      name: "AES-GCM",
+      iv: iv,
+    },
+    key,
+    cipherText
+  );
+};
+
+export const getDigest = (uid) => {
+  let enc = new TextEncoder();
+  return crypto.subtle.digest("SHA-256", enc.encode(uid));
+};
+
+export const startEncryption = (file, password, setIv) => {
+  console.log(file, password);
+  let digest = getDigest(password);
+  let rawFile = getFile(file);
+  let iv = getiv();
+  Promise.all([digest, iv, rawFile]).then((values) => {
+    setIv(iv);
+    console.log(values);
+    // generate a crypto key
+    getKey(values[0]).then((resp) => {
+      encryptFile(resp, values[1], values[2]).then((cipherText) => {
+        let fileBlob = new Blob([cipherText], { type: file.type });
+        FileSaver.saveAs(fileBlob, file.name);
+      });
+    });
+  });
+};
+
+export const startDecryption = (cipherText, password, iv) => {
+  let digest = getDigest(password);
+  let rawFile = getFile(cipherText);
+  // let iv = getiv();
+  Promise.all([digest, "", rawFile]).then((values) => {
+    // generate a crypto key
+    getKey(values[0]).then((resp) => {
+      decryptFile(resp, iv, values[2]).then((file) => {
+        console.log(file);
+        let fileBlob = new Blob([file], { type: file.type });
+        FileSaver.saveAs(fileBlob, file.name);
+      });
+    });
+  });
+};
